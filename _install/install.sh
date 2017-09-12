1dfb9a1513c3ac34dafdf7da6a186e3817654ba2
sto <sto@usility.ch>
2017-09-12 23:27:07 +0200

    initial commit

---
+++ _install/install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+echo Lizzy has not been installed yet.
+
+echo ""
+
+echo "Now installing to:"
+echo "  `pwd`"
+
+echo ""
+
+echo "  -> Copying files to root of application..."
+# copy basic folders to root of app
+cp -R _lizzy/_parent-folder/* .
+
+echo ""
+
+
+echo "  -> Activating .htaccess..."
+# activate the .htaccess file
+mv htaccess .htaccess
+
+echo ""
+
+echo Installation successful!
+
+echo ""
+
+echo Please reload the page now.
