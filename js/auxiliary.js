c1d660af5915b3964998a1d908510f9ecfaafa2c
sto <sto@usility.ch>
2019-01-20 12:49:01 +0100

    jQ Module added: selText()

--- js/auxiliary.js
+++ js/auxiliary.js
@@ -109,3 +109,27 @@ $( document ).ready(function() {
 });
 */
 
+
+//--------------------------------------------------------------
+// plug-in to get specified element selected
+jQuery.fn.selText = function() {
+    this.find('input').each(function() {
+        if($(this).prev().length == 0 || !$(this).prev().hasClass('p_copy')) {
+            $('<p class="p_copy" style="position: absolute; z-index: -1;"></p>').insertBefore($(this));
+        }
+        $(this).prev().html($(this).val());
+    });
+    var doc = document;
+    var element = this[0];
+    if (doc.body.createTextRange) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        var selection = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
