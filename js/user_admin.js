474cf797217d94ac493137ef0a7e2e54dafea56e
sto <sto@usility.ch>
2019-02-05 08:16:25 +0100

    cleanup admin-submit button to remove url arguments (to avoid initiating admin activities)

--- js/user_admin.js
+++ js/user_admin.js
@@ -90,38 +90,11 @@ function setupOnetimeLogin() {
 
 
 
-    /*
-    $('#info-onetimelogin').click(function(e) {
-        $('#info-onetimelogin-text').toggleClass('dispno');
-    });
-
-
-    $('#info-onetimelogin2').click(function(e) {
-        $('#info-onetimelogin-text2').toggleClass('dispno');
-    });
-
-
-    $('#password-alternative').click(function(e) {
-        $('#info-password-alternative-text').toggleClass('dispno');
-    });
-
-
-    $('#one-time-access-link').click(function(e) {
-        e.preventDefault();
-        var loginEmail = $('#fld_username').val();
-        if (loginEmail.match(/[^@]+@[^@]+\.\w+/)) {
-            console.log('email address looks good');
-        } else {
-            $('#lzy-lbl-login-user .lzy-err-msg').text('{{ email required for one-time-access-link }}');
-            $('#fld_username').focus();
-        }
-    });
-    */
 
     $('.lzy-admin-submit-button').click(function(e) {
         e.preventDefault();
         $(this).prop('disabled', true);
-        var url = window.location.href;
+        var url = window.location.href.replace(/\?.*/, '');
         if (!(url.match(/^https:\/\//) || url.match(/^http:\/\/localhost/) || url.match(/^http:\/\/192\.168/))) {
             alert('{{ Warning insecure connection }}');
             return;
@@ -144,110 +117,7 @@ function setupOnetimeLogin() {
 
     });
 
-/*
-    $('.lzy-signup-submit-button').click(function(e) {
-        e.preventDefault();
-        $( this ).prop('disabled', true);
-        var url = window.location.href;
-        if (!(url.match(/^https:\/\//) || url.match(/^http:\/\/localhost/) || url.match(/^http:\/\/192\.168/) )) {
-            alert('{{ Warning insecure connection }}');
-            return;
-        }
-        var $form = $( this ).closest('form');
-        $form.attr('action', url);
 
-        $(".lzy-login-form output").text('');
-        // $("#lzy-lbl-login-user output").text('');
-        $(".lzy-form-password output").text('');
-        var email = $('.lzy-login-email').val();
-        if (!email) {
-            $(".lzy-error-message").text('{{ Err empty email }}');
-            $( this ).prop('disabled', false);
-            return;
-        }
-        if (false && (location.protocol != 'https:')) {
-            alert('No HTTPS Connection!');
-            return;
-        }
-        $form.submit();
-    });
-
-
-    $('.lzy-add-user-submit-button').click(function(e) {
-        e.preventDefault();
-        $( this ).prop('disabled', true);
-        var url = window.location.href;
-        if (!(url.match(/^https:\/\//) || url.match(/^http:\/\/localhost/) || url.match(/^http:\/\/192\.168/) )) {
-            alert('{{ Warning insecure connection }}');
-            return;
-        }
-        var $form = $( this ).closest('form');
-        $form.attr('action', url);
-
-        $(".lzy-login-form output").text('');
-        // $("#lzy-lbl-login-user output").text('');
-        $(".lzy-form-password output").text('');
-        var email = $('.lzy-textarea').val();
-        if (!email) {
-            $(".lzy-error-message").text('{{ Err empty emails }}');
-            $( this ).prop('disabled', false);
-            return;
-        }
-        if (false && (location.protocol != 'https:')) {
-            alert('No HTTPS Connection!');
-            return;
-        }
-        $form.submit();
-    });
-
-
-
-    // $('#btn_lzy-login-submit2').click(function(e) {
-    $('.lzy-login-submit-button').click(function(e) {
-        e.preventDefault();
-        $( this ).prop('disabled', true);
-        var url = window.location.href;
-        if (!(url.match(/^https:\/\//) || url.match(/^http:\/\/localhost/) || url.match(/^http:\/\/192\.168/) )) {
-            $( this ).prop('disabled', false);
-            alert('{{ Warning insecure connection }}');
-            return;
-        }
-        var $form = $( this ).closest('form');
-        $form.attr('action', url);
-        // $('.lzy-login-form form').attr('action', url);
-        // $(".lzy-login-form output").text('');
-        // $(".lzy-form-password output").text('');
-
-        if ($( this ).closest('.lzy-login-by-email').length) {
-            var em = $('.lzy-login-email').val();
-            if (!em) {
-                $(this).prop('disabled', false);
-                $('.lzy-error-message').show();
-                return;
-            }
-
-        } else {
-            var un = $('.lzy-login-username').val();
-            var pw = $('.lzy-form-password').val();
-            if (!un) {
-                $(this).prop('disabled', false);
-                return;
-            }
-            if (!pw) {
-                $('.lzy-form-password').focus();
-                $(this).prop('disabled', false);
-                return;
-            }
-            if (false && (location.protocol != 'https:')) {
-                alert('No HTTPS Connection!');
-                return;
-            }
-        }
-        $form.submit();
-
-    });
-
-*/
     $('.lzy-login-tab-label1').click(function() {
         setTimeout(function(){ $('#login_email').focus(); }, 20);
     });
