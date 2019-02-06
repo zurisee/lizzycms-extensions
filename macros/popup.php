8ca50271a2f8201a4e7d9ce5848cc56b492661b1
sto <sto@usility.ch>
2019-02-06 08:35:41 +0100

    cleanup

--- macros/popup.php
+++ macros/popup.php
@@ -7,12 +7,9 @@ $macroName = basename(__FILE__, '.php');    // macro name normally the same as t
 
 $this->addMacro($macroName, function () {
     $macroName = basename(__FILE__, '.php');
-    $this->invocationCounter[$macroName] = (!isset($this->invocationCounter[$macroName])) ? 0 : ($this->invocationCounter[$macroName]+1);
 
-//    $inx = $this->invocationCounter[$macroName] + 1;
     $args = $this->getArgsArray($macroName);
 
     return $this->page->addPopup($args);
-//    return $this->page->popupInstance->addPopup($args);
 });
 
