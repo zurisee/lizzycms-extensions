826c856a16700938240fe25fb2e5b3e1d6195dcb
sto <sto@usility.ch>
2019-03-03 03:38:16 +0200

    option "unambiguous" added -> avoids zero and 'O' in hash code

--- ticketing.class.php
+++ ticketing.class.php
@@ -9,6 +9,7 @@ define ('DEFAULT_TICKET_VALIDITY_TIME', 900);
  * $validityPeriod:
  *      null = system default
  *      0    = infinite
+ *      string
  */
 
 class Ticketing
@@ -18,6 +19,7 @@ class Ticketing
         $dataSrc = isset($options['dataSrc']) ? $options['dataSrc'] : DEFAULT_TICKET_STORAGE_FILE;
         $this->hashSize = isset($options['hashSize']) ? $options['hashSize'] : DEFAULT_TICKET_HASH_SIZE;
         $this->defaultType = isset($options['defaultType']) ? $options['defaultType'] : 'generic';
+        $this->unambiguous = isset($options['unambiguous']) ? $options['unambiguous'] : false;
         $this->defaultValidityPeriod = isset($options['defaultValidityPeriod']) ? $options['defaultValidityPeriod'] : DEFAULT_TICKET_VALIDITY_TIME;
         $this->ds = new DataStorage($dataSrc, '', true);
         $this->purgeExpiredTickets();
@@ -37,6 +39,10 @@ class Ticketing
 
         } elseif (($validityPeriod === false) || ($validityPeriod <= 0)) {
             $ticket['lzy_ticketValidTill'] = PHP_INT_MAX;
+
+        } elseif (is_string($validityPeriod)) {
+            $ticket['lzy_ticketValidTill'] = strtotime( $validityPeriod );
+            mylog('ticket till: '.date('Y-m-d', $ticket['lzy_ticketValidTill']));
 
         } else {
             $ticket['lzy_ticketValidTill'] = time() + $validityPeriod;
@@ -61,44 +67,46 @@ class Ticketing
     public function consumeTicket($ticketHash, $type = false)
     {
         $this->ds->lock($ticketHash);
-        $ticket = $this->ds->read($ticketHash);
+        $ticketRec = $this->ds->read($ticketHash);
 
-        if ($type && ($type !== $ticket['lzy_ticketType'])) {
-            $ticket = false;
+        if ($type && ($type !== $ticketRec['lzy_ticketType'])) {
+            $ticketRec = false;
 
-        } elseif ($ticket['lzy_ticketValidTill'] < time()) {      // ticket expired
+        } elseif ($ticketRec['lzy_ticketValidTill'] < time()) {      // ticket expired
             $this->ds->delete($ticketHash);
-            $ticket = false;
+            $ticketRec = false;
 
         } else {
-            $n = $ticket['lzy_maxConsumptionCount'];
+            $n = $ticketRec['lzy_maxConsumptionCount'];
             if ($n > 1) {
-                $ticket['lzy_maxConsumptionCount'] = $n - 1;
-                $this->ds->write($ticketHash, $ticket);
+                $ticketRec['lzy_maxConsumptionCount'] = $n - 1;
+                $this->ds->write($ticketHash, $ticketRec);
                 $this->ds->unlock($ticketHash);
             } else {
                 $this->ds->delete($ticketHash);
             }
 
-            $lzy_ticketType = $ticket['lzy_ticketType'];
+            $lzy_ticketType = $ticketRec['lzy_ticketType'];
 
-            unset($ticket['lzy_maxConsumptionCount']);  // don't return private properties
-            unset($ticket['lzy_ticketValidTill']);
+            unset($ticketRec['lzy_maxConsumptionCount']);  // don't return private properties
+            unset($ticketRec['lzy_ticketValidTill']);
 
             if ($lzy_ticketType === 'sessionVar') {     // type 'sessionVar': make ticket available in session variable
-                $_SESSION['lizzy']['ticket'] = $ticket;
+                $_SESSION['lizzy']['ticket'] = $ticketRec;
             }
-            unset($ticket['lzy_ticketType']);
+            unset($ticketRec['lzy_ticketType']);
         }
-        return $ticket;
+        return $ticketRec;
     } // consumeTicket
 
 
 
     private function createHash()
     {
-        $hash = chr(random_int(65, 90));  // first always a letter
-        $hash .= strtoupper(substr(sha1(random_int(0,  PHP_INT_MAX)), 0, $this->hashSize-1));  // letters and digits
+        do {
+            $hash = chr(random_int(65, 90));  // first always a letter
+            $hash .= strtoupper(substr(sha1(random_int(0, PHP_INT_MAX)), 0, $this->hashSize - 1));  // letters and digits
+        } while ($this->unambiguous && strpbrk($hash, '0O'));
         return $hash;
     } // createHash
 
