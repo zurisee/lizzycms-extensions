228e57f45a73ff585bbf7ff8226a8ccdabeb27e8
sto <sto@usility.ch>
2019-10-17 12:50:04 +0200

    new macro: last-modified() -> just an alias for filedate(), which is a bad name but kept for compatibility

---
+++ macros/last-modified.php
@@ -0,0 +1,40 @@
+<?php
+// @info: From a selection of files determines the last modified and returns the timestamp
+
+$macroName = basename(__FILE__, '.php');
+
+$this->addMacro($macroName, function () {
+    $macroName = basename(__FILE__, '.php');
+    $files = $this->getArg($macroName, 'files', 'Files that will be checked to determine the newest. Use "glob syntax", e.g. "&#126;/data/*". Separate multiple elements by comma.', '~page/*');
+    $format = $this->getArg($macroName, 'format', 'Format in which the output will be rendered (see http://php.net/manual/en/function.date.php)', 'Y-m-d');
+    $recursive = $this->getArg($macroName, 'recursive', 'If true, files in sub-folders will be included.)', false);
+    $exclude = $this->getArg($macroName, 'exclude', 'Regex-pattern of elements to be excluded.', false);
+    $file = $this->getArg($macroName, 'file', 'Synonym for "files"', '');
+
+    if ($files === 'help') {
+        return;
+    }
+
+    if ($file) {
+        $files = $file;
+    }
+    if (preg_match('/^\[(.*)\]$/', $files, $m)) {
+        $files = $m[1];
+    }
+    $filePaths = explode(',', $files);
+
+    $newest = 0;
+    foreach ($filePaths as $path) {
+        $path = makePathRelativeToPage($path);
+        $newest = max( lastModified($path, $recursive, $exclude), $newest);
+    }
+    if ($newest === 0) {
+        $filedate = '{{ unknown }}';
+    } else {
+        $filedate = date($format, $newest);
+    }
+    $this->optionAddNoComment = true;
+	return $filedate;
+});
+
+
