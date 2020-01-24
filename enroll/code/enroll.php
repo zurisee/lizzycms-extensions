749d5de80b4931cfc2bf9432744be1862a46d5b0
Dieter Stokar <zurisee@gmail.com>
2020-01-24 17:19:16 +0100

    extension enroll() -> functionality modified: - optional field 'phone' removed (can now be invoked explicitly as a custom field) - error message presentation -> now as page-message - logging improved to include error msg

--- enroll/code/enroll.php
+++ enroll/code/enroll.php
@@ -61,7 +61,6 @@ class enroll
 		$this->err_msg = '';
 		$this->name = '';
 		$this->email = '';
-		$this->phone = '';
 		$this->action = '';
 		$this->focus = '';
 		$this->show_result = false;
@@ -85,9 +84,9 @@ class enroll
             $customFields = explodeTrim(',|', $this->customFields);
             $customFields = "\t".implode("\t", $customFields);
             if ($logAgentData) {
-                file_put_contents($this->logFile, "Timestamp\tAction\tList\tName\tEmail\tPhone$customFields\tClient\tIP\n");
+                file_put_contents($this->logFile, "Timestamp\tAction\tList\tName\tEmail$customFields\tClient\tIP\n");
             } else {
-                file_put_contents($this->logFile, "Timestamp\tAction\tList\tName\tEmail\tPhone$customFields\n");
+                file_put_contents($this->logFile, "Timestamp\tAction\tList\tName\tEmail$customFields\n");
             }
 		}
 		$this->handle_post_data();
@@ -115,9 +114,8 @@ class enroll
 				$admin_mode = 'admin ';
 			}
 			$email = strtolower(get_post_data('lzy-enroll-email'));
-			$phone = get_post_data('lzy-enroll-phone');
 
-			$out = "\t$admin_mode$action\t{$this->enroll_list_id}\t$name\t$email\t$phone";
+			$out = "\t$admin_mode$action\t{$this->enroll_list_id}\t$name\t$email";
 
 			if ($this->customFields) {
                 $customFields = explodeTrim(',|', $this->customFields);
@@ -130,19 +128,14 @@ class enroll
 			    $i++;
             }
 
-            if ($this->logAgentData) {
-                file_put_contents($this->logFile, timestamp()."$out\t{$_SERVER["HTTP_USER_AGENT"]}\t{$_SERVER['REMOTE_ADDR']}\n", FILE_APPEND);
-            } else {
-                file_put_contents($this->logFile, timestamp()."$out\n", FILE_APPEND);
-            }
-
-			if (!is_legal_email_address($email) && !$this->admin_mode) {
+            if (!is_legal_email_address($email) && !$this->admin_mode) {
 				writeLog("\tError: illegal email address [$name] [$email]");
 				$this->err_msg = '{{ illegal email address }}';
 				$this->name = $name;
 				$this->email = $email;
-				$this->phone = $phone;
 				$this->focus = 'email';
+                $this->enrollLog($out);
+                return;
 			}
 			$_POST = array();
 			$file = $this->dataFile;
@@ -155,28 +148,39 @@ class enroll
 			$entry = &$enrollData[$id];
 			if ($action === 'add') {
 				$this->action = 'add';
-				if (!$this->err_msg) {
-					foreach ($entry as $n => $rec) {
-						if ($rec['Name'] === $name){ // name already exists
-							writeLog("\tError: enrolling twice [$name] [$email]");
-							$this->err_msg = '{{ enroll entry already exists }}';
-							$this->name = $name;
-							$this->email = $email;
-							$this->phone = $phone;
-							$this->focus = 'name';
-							return;
-						}
-					}
-					$n = sizeof($entry);
-					$entry[$n]['Name'] = $name;
-					$entry[$n]['EMail'] = $email;
-					$entry[$n]['Phone'] = $phone;
-					$entry[$n]['time'] = time();
+                // check whether nane already entered:
+                foreach ($entry as $n => $rec) {
+                    if ($rec['Name'] === $name){ // name already exists:
+                        if ($entry[$n]['EMail'] === $email) {   // with same email, so we let it pass:
+                            $entry[$n]['Name'] = $name;
+                            $entry[$n]['EMail'] = $email;
+                            $entry[$n]['time'] = time();
+                            foreach ($customFields as $i => $field) {
+                                $entry[$n][$field] = $customFieldValues[$i];
+                            }
+                            $found = true;
+                            break;
+                        } else {  // existing name but different email -> reject:
+                            writeLog("\tError: enrolling twice [$name] [$email]");
+                            $this->err_msg = '{{ enroll entry already exists }}';
+                            $this->name = $name;
+                            $this->email = $email;
+                            $this->focus = 'name';
+                            $this->enrollLog($out);
+                            return;
+                        }
+                    }
+                }
+                if (!isset($found)) {   // new entry:
+                    $n = sizeof($entry);
+                    $entry[$n]['Name'] = $name;
+                    $entry[$n]['EMail'] = $email;
+                    $entry[$n]['time'] = time();
                     foreach ($customFields as $i => $field) {
                         $entry[$n][$field] = $customFieldValues[$i];
                     }
-				}
-	
+                }
+
 			} elseif ($action === 'delete') {
 				$this->action = 'delete';
 				$found = false;
@@ -196,7 +200,6 @@ class enroll
 								$this->err_msg = '{{ enroll email wrong }}';
 								$this->name = $name;
 								$this->email = $email;
-								$this->phone = $phone;
 								$this->focus = 'email';
 							} else {
 								$found = true;
@@ -207,7 +210,6 @@ class enroll
 							$this->err_msg = '{{ enroll entry too old }}';
 							$this->name = $name;
 							$this->email = $email;
-							$this->phone = $phone;
 						}
 						$found = true;
 					}
@@ -220,13 +222,13 @@ class enroll
 					$this->err_msg = '{{ no enroll entry found }}';
 					$this->name = $name;
 					$this->email = $email;
-					$this->phone = $phone;
 				}
 			} else {
 				return; // nothing to do
 			}
-	
-			$yaml = convertToYaml($enrollData);
+
+            $this->enrollLog($out);
+            $yaml = convertToYaml($enrollData);
 			file_put_contents($this->dataFile, $yaml);
 		}
 	} // handle $_POST
@@ -273,10 +275,8 @@ class enroll
             if (isset($entry[$n]['Name'])) {	// Name exists -> delete
 				$name =  $entry[$n]['Name'];
 				$email = $entry[$n]['EMail'];
-				$phone = $entry[$n]['Phone'];
 				if ($this->admin_mode) {
 					$name .= " &lt;$email>";
-					if ($phone) {$name .= " $phone"; }
 					$tooltip = $name;
 				}
 				if ((intval($entry[$n]['time']) > time()-(3600*24)) || $this->admin_mode) {
@@ -307,6 +307,9 @@ class enroll
 		}
 	
 		$out .= "\t</div> <!-- /lzy-enrollment-list -->\n  ";
+		if ($this->err_msg) {
+		    $this->trans->page->addMessage($this->err_msg);
+        }
 
 		return $out;
 	} // m_enroll
@@ -327,7 +330,6 @@ class enroll
 			if (dialog_id === '#addDialog') {
 				$('#lzy-add-name', \$form).val('{$this->name}');
 				$('#lzy-add-email', \$form).val('{$this->email}');
-				$('#lzy-add-phone', \$form).val('{$this->phone}');
 				$('#lzy-add-list-id', \$form).val('{$this->enroll_list_id}');
 				$('#lzy-enroll-add-type', \$form).val('{$this->action}');
 				var dialog_title = '{$this->enroll_list_name}';
@@ -339,7 +341,6 @@ class enroll
 			} else if (dialog_id === '#delDialog') {
 				$('#lzy-del-name', \$form).val('{$this->name}');
 				$('#lzy-del-email', \$form).val('{$this->email}');
-				$('#d_phone', \$form).val('{$this->phone}');
 				$('#lzy-add-list-id', \$form).val('{$this->enroll_list_id}');
 				$('#lzy-enroll-del-type', \$form).val('{$this->action}');
 				var dialog_title = '{$this->enroll_list_name}';
@@ -471,9 +472,6 @@ EOT;
 		$err_msg = '';
         $customFields = $this->renderCustomFields();
 
-		if (($this->action == 'add') && $this->err_msg) {
-			$err_msg = "\n\t<div class='lzy-err-msg'>$this->err_msg</div>";
-		}
 		$reqScheme = (isset($_SERVER['REQUEST_SCHEME'])) ? $_SERVER['REQUEST_SCHEME'] : 'http';
 		$url = $reqScheme.'://'.$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
 		$form = <<<EOT
@@ -491,8 +489,6 @@ EOT;
             <label for="lzy-add-email" class="ui-hidden-accessible">E-Mail:</label>
             <input type="text" name="lzy-enroll-email" id="lzy-add-email" value="" placeholder="name@domain.net" data-theme="a" required aria-required="true" />
 
-            <label for="lzy-phone" class="ui-hidden-accessible">Handy:</label>
-            <input type="text" name="lzy-enroll-phone" id="lzy-phone" value="" placeholder="{{ placeholder mobile number }}" data-theme="a" />
 $customFields
             <button type="submit" id="a_submit" class="ui-btn ui-corner-all ui-shadow ui-btn-b ui-btn-icon-left ui-icon-check">{{ Enroll now }}</button>
             <button type="cancel" id="a_cancel" class="ui-btn ui-corner-all ui-shadow ui-btn-b ui-btn-icon-left ui-icon-check">{{ Cancel }}</button>
@@ -556,9 +552,6 @@ EOT;
 	//------------------------------------
 	private function create_delete_dialog() {
 		$err_msg = '';
-		if (($this->action == 'delete') && $this->err_msg) {
-			$err_msg = "\n\t<div class='lzy-err-msg'>$this->err_msg</div>";
-		}
         $reqScheme = (isset($_SERVER['REQUEST_SCHEME'])) ? $_SERVER['REQUEST_SCHEME'] : 'http';
         $url = $reqScheme.'://'.$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
 		$form = <<<EOT
@@ -588,6 +581,22 @@ EOT;
 EOT;
 		return $form;
 	} // create_delete_dialog
+
+
+
+
+    private function enrollLog($out)
+    {
+        $err = '';
+        if ($this->err_msg) {
+            $err = "\tError: {$this->err_msg}";
+        }
+        if ($this->logAgentData) {
+            file_put_contents($this->logFile, timestamp() . "$out\t{$_SERVER["HTTP_USER_AGENT"]}\t{$_SERVER['REMOTE_ADDR']}$err\n", FILE_APPEND);
+        } else {
+            file_put_contents($this->logFile, timestamp() . "$out$err\n", FILE_APPEND);
+        }
+    }
 
 } // class enroll
 
