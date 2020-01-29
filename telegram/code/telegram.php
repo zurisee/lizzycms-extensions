9a2fd31f62356f3eaa089f20952c21b696ffe40e
sto <sto@usility.ch>
2020-01-29 16:44:16 +0100

    new macro: telegram() - based on MadelineProto (https://docs.madelineproto.xyz) - permits to send messages via telegram app (https://telegram.org) - automatically downloads latest .phar - if no 'session' is found, walks through the processes of linking to a telegram-bot

---
+++ telegram/code/telegram.php
@@ -0,0 +1,49 @@
+<?php
+
+// initialize MadelineProto:
+$pwd = getcwd();
+$cwd = __DIR__;
+chdir($cwd);
+include 'madeline.php';
+chdir($pwd);
+
+$macroName = basename(__FILE__, '.php');    // macro name normally the same as the file name
+
+
+$this->addMacro($macroName, function () {
+	$macroName = basename(__FILE__, '.php');
+	$this->invocationCounter[$macroName] = (!isset($this->invocationCounter[$macroName])) ? 0 : ($this->invocationCounter[$macroName]+1);
+    $str = '';
+
+    $to = $this->getArg($macroName, 'to', 'A Telegram peer id, e.g. "@buddy".', '');
+    $msg = $this->getArg($macroName, 'msg', 'The message string to be sent.', '');
+    $from = $this->getArg($macroName, 'from', 'An optional id-string (in case you want to use multiple telegram bots in parallel). E.g. "bot1" (use any string you like).', '');
+
+    if ($to === 'help') {
+        return '';
+    }
+    if ($to && $msg) {
+        if ($from) {
+            $sessionFile = "data/telegram/$from.session.madeline";
+        } else {
+            $sessionFile = 'data/telegram/session.madeline';
+        }
+        $str = SendTelegram($sessionFile, $to, $msg);
+    }
+
+    return $str;
+});
+
+
+
+
+
+function SendTelegram($sessionFile, $to, $msg)
+{
+    $MadelineProto = new \danog\MadelineProto\API($sessionFile);
+    $MadelineProto->async(true);
+    $MadelineProto->loop(function () use ($MadelineProto, $to, $msg) {
+        yield $MadelineProto->start();
+        yield $MadelineProto->messages->sendMessage(['peer' => $to, 'message' => $msg]);
+    });
+}
