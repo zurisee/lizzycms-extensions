869e0bbf1b5fd0709ac48a62f288cea5d6e6f0bf
Dieter Stokar <zurisee@gmail.com>
2020-03-02 18:09:36 +0100

    bugfix: constant MKDIR_MASK undefined in backend

--- editable/backend/_editable_backend.php
+++ editable/backend/_editable_backend.php
@@ -45,7 +45,9 @@ define('PATH_TO_APP_ROOT', 	SYSTEM_PATH.'../');		                // root folder 
 
 define('LOCK_TIMEOUT', 		120);	                                // max time till field is automatically unlocked
 define('MAX_URL_ARG_SIZE',  255);
-//define('MKDIR_MASK',        0700);                                  // remember to modify _lizzy/_install/install.sh as well
+if (!defined('MKDIR_MASK')) {
+    define('MKDIR_MASK', 0700);
+}
 
 require_once SYSTEM_PATH.'backend_aux.php';
 require_once SYSTEM_PATH.'datastorage2.class.php';
