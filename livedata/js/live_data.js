fc8048921d21cc2f308828e394e7e47debdac0b9
sto <sto@usility.ch>
2020-12-25 14:03:31 +0100

    code overhaul: turned into object LiveData()

--- livedata/js/live_data.js
+++ livedata/js/live_data.js
@@ -1,232 +1,254 @@
 // LiveData
 //  Note: requires explicit invokation in livedata.php, resp. editable module
 
-var ajaxHndl = null;
-var lastUpdated = 0;
-var paramName = '';
-var paramSource = '';
-var prevParamValue = '';
-var refs = '';
-var debugOutput = false;
+"use strict";
 
+var LiveData = new Object({
+    ajaxHndl: null,
+    lastUpdated: 0,
+    paramName: '',
+    paramSource: '',
+    prevParamValue: '',
+    refs: '',
+    debugOutput: false,
 
-function initLiveData( execInitialUpdate ) {
-    // collect all references within page:
-    $('[data-lzy-data-ref]').each(function () {
-        var ref = $( this ).attr('data-lzy-data-ref');
-        refs = refs + ref + ',';
-    });
-    refs = refs.replace(/,+$/, '');
-
-
-    // check for dynamic parameter: (only apply first appearance)
-    $('[data-live-data-param]').each(function () {
-        var paramDef = $( this ).attr('data-live-data-param');
-        paramDef = paramDef.split('=');
-        paramName = paramDef[0];
-        paramSource = paramDef[1];
-        console.log('custom param: ' + paramSource + ' => ' + paramName);
-        return false;
-    });
-
-    debugOutput = ($('.debug').length !== 0);
-
-    if ((typeof execInitialUpdate !== 'undefined') && !execInitialUpdate) {
-        lastUpdated = -1;
-    }
-
-    updateLiveData(  );
-// updateLiveData( true );
-} // init
-
-
-
-
-function markLockedFields( lockedElements ) {
-    $('.lzy-element-locked').removeClass('lzy-element-locked');
-
-    for (var i in lockedElements) {
-        var targSel = lockedElements[ i ];
-        if (targSel === '*') {
-            $('.lzy-live-data').addClass('lzy-element-locked');
-        } else {
-            $( targSel ).addClass('lzy-element-locked');
-        }
-    }
-} // markLockedFields
-
-
-
-
-function markFrozenFields( frozenElements ) {
-    $('.lzy-element-locked').removeClass('lzy-element-locked');
-
-    for (var i in frozenElements) {
-        var targSel = frozenElements[ i ];
-        if (targSel === '*') {
-            $('.lzy-live-data').addClass('lzy-element-frozen');
-        } else {
-            $( targSel ).addClass('lzy-element-frozen');
-        }
-    }
-} // markFrozenFields
-
-
-
-
-function updateDOM(data) {
-    if (typeof data.locked !== 'undefined') {
-        markLockedFields(data.locked);
-    }
-
-    if (typeof data.frozen !== 'undefined') {
-        markFrozenFields(data.frozen);
-    }
-
-    for (var targSel in data.data) {
-        var val = data.data[targSel];
-        //mylog(`writing to '${targSel}' value '${val}'`);
-        $( targSel ).each(function() {
-            var $targ = $( this );
-            const id = $targ.attr('id');
-
-            // skip element if it's in active editable mode:
-            if ($targ.hasClass('lzy-editable-active')) {
-                mylog('live-data: skipping active editable field: ' + id);
-                return;
-            }
-
-            var goOn = true;
-// var callback = $('.lzy-live-data').attr('data-live-callback');
-            var callback = $targ.attr('data-live-callback');
-            if (typeof window[callback] === 'function') {
-                goOn = window[callback]( targSel, val );
-            }
-
-            if (goOn) {
-                var tag = $targ.prop('tagName');
-                var valTags = 'INPUT,SELECT,TEXTAREA';
-                if (valTags.includes(tag)) {
-                    $targ.val(val);
-                } else {
-                    $targ.html(val);
-                }
-            }
-
-            var postCallback = $targ.attr('data-live-post-update-callback');
-            if (typeof window[postCallback] === 'function') {
-                window[postCallback]( targSel, val );
-            }
+    init: function( execInitialUpdate ) {
+        var rootObj = this;
+        // collect all references within page:
+        $('[data-lzy-data-ref]').each(function () {
+            var ref = $( this ).attr('data-lzy-data-ref');
+            rootObj.refs = rootObj.refs + ref + ',';
         });
-        if (debugOutput) {
-            console.log(targSel + ' -> ' + val);
-        }
-    }
-// var postCallback = $('.lzy-live-data').attr('data-live-post-update-callback');
-// if (typeof window[postCallback] === 'function') {
-//     window[postCallback]( data.data );
-// }
-} // updateDOM
+        this.refs = this.refs.replace(/,+$/, '');
 
 
+        // check for dynamic parameter: (only apply first appearance)
+        $('[data-live-data-param]').each(function () {
+            var paramDef = $( this ).attr('data-live-data-param');
+            paramDef = paramDef.split('=');
+            rootObj.paramName = paramDef[0];
+            rootObj.paramSource = paramDef[1];
+            console.log('custom param: ' + rootObj.paramSource + ' => ' + rootObj.paramName);
+            return false;
+        });
 
+        this.debugOutput = ($('.debug').length !== 0);
 
-function handleAjaxResponse(json) {
-    ajaxHndl = null;
-    console.log('ajax: ' + json);
-    if (!json) {
-        console.log('No data received - terminating live-data');
-        return;
-    }
-    json = json.replace(/(.*[\]}])\#.*/, '$1');    // remove trailing #comment
-    try {
-        var data = JSON.parse(json);
-    } catch (e) {
-        console.log('Error condition detected - terminating live-data');
-        console.log(json);
-        return false;
-    }
-
-    if (typeof data.lastUpdated !== 'undefined') {
-        lastUpdated = data.lastUpdated;
-    }
-    if (typeof data.result === 'undefined') {
-        console.log('_live_data_service.php reported an error');
-        console.log(json);
-        return;
-    }
-
-    // regular response:
-    if (typeof data.data === 'undefined') {
-        if (debugOutput) {
-            console.log(timeStamp() + ': No new data');
+        if ((typeof execInitialUpdate !== 'undefined') && !execInitialUpdate) {
+            this.lastUpdated = -1;
         }
 
-    } else {
-        var goOn = true;
-        if (typeof liveDataCallback === 'function') {
-            goOn = liveDataCallback( data.data );
+        this.updateLiveData(  );
+    }, // init
+
+
+
+
+    markLockedFields: function( lockedElements ) {
+        $('.lzy-element-locked').removeClass('lzy-element-locked');
+
+        if (typeof lockedElements !== 'object') {
+            return;
         }
-        if (goOn) {
-            updateDOM(data);
+        for (var i in lockedElements) {
+            var targSel = lockedElements[i];
+            if (targSel === '*') {
+                $('.lzy-live-data').addClass('lzy-element-locked');
+            } else {
+                $(targSel).addClass('lzy-element-locked');
+            }
         }
-        if (typeof liveDataPostUpdateCallback === 'function') {
-            goOn = liveDataPostUpdateCallback( data.data );
+    }, // markLockedFields
+
+
+
+
+    markFrozenFields: function( frozenElements ) {
+        $('.lzy-element-locked').removeClass('lzy-element-locked');
+
+        if (typeof frozenElements !== 'object') {
+            return;
         }
-    }
-    $('.live-data-update-time').text(timeStamp());
-    updateLiveData();
-} // handleAjaxResponse
-
-
-
-
-function updateLiveData() {
-    var url = appRoot + "_lizzy/extensions/livedata/backend/_live_data_service.php";
-    if (paramName) {
-        var paramValue = $( paramSource ).text();
-        url = appendToUrl(url, 'dynDataSel=' + paramName + ':' + paramValue);
-        if (paramValue !== prevParamValue) {
-            lastUpdated = 0;
-            prevParamValue = paramValue;
+        for (var i in frozenElements) {
+            var targSel = frozenElements[i];
+            if (targSel === '*') {
+                $('.lzy-live-data').addClass('lzy-element-frozen');
+            } else {
+                $(targSel).addClass('lzy-element-frozen');
+            }
         }
-    }
-
-    if (ajaxHndl !== null){
-        ajaxHndl.abort();
-    }
-    ajaxHndl = $.ajax({
-        url: url,
-        type: 'POST',
-        data: { ref: refs, last: lastUpdated },
-        async: true,
-        cache: false,
-    }).done(function ( json ) {
-        return handleAjaxResponse(json);
-    });
-} // update
+    }, // markFrozenFields
 
 
 
 
-$(window).bind('beforeunload', function(e) {
-    abortAjax();
+    updateDOM: function( data ) {
+        if (typeof data.locked !== 'undefined') {
+            this.markLockedFields(data.locked);
+        }
+
+        if (typeof data.frozen !== 'undefined') {
+            this.markFrozenFields(data.frozen);
+        }
+
+        if (typeof data.data !== 'object') {
+            return;
+        }
+        var $targ = null;
+        for (var targSel in data.data) {
+            var val = data.data[targSel];
+            //mylog(`writing to '${targSel}' value '${val}'`);
+            $( targSel ).each(function() {
+                $targ = $( this );
+                const id = $targ.attr('id');
+
+                // skip element if it's in active editable mode:
+                if ($targ.hasClass('lzy-editable-active')) {
+                    mylog('live-data: skipping active editable field: ' + id);
+                    return;
+                }
+
+                var goOn = true;
+                var callback = $targ.attr('data-live-callback');
+                if (typeof window[callback] === 'function') {
+                    goOn = window[callback]( targSel, val );
+                }
+
+                if (goOn) {
+                    var tag = $targ.prop('tagName');
+                    var valTags = 'INPUT,SELECT,TEXTAREA';
+                    if (valTags.includes(tag)) {
+                        $targ.val(val);
+                    } else {
+                        $targ.html(val);
+                    }
+                }
+            });
+
+            if (this.debugOutput) {
+                console.log(targSel + ' -> ' + val);
+            }
+        }
+
+        if ($targ) {
+            var postCallback = $targ.attr('data-live-post-update-callback');
+            if (typeof postCallback === 'undefined') {
+                postCallback = $targ.closest('[data-live-post-update-callback]').attr('data-live-post-update-callback');
+            }
+            if (typeof postCallback === 'undefined') {
+                postCallback = $('[data-live-post-update-callback]').attr('data-live-post-update-callback');
+            }
+            if ((typeof postCallback !== 'undefined') && (typeof window[postCallback] === 'function')) {
+                window[postCallback]( data.data );
+            }
+        }
+    }, // updateDOM
+
+
+
+
+    handleAjaxResponse: function( json ) {
+        this.ajaxHndl = null;
+        var data = null;
+        console.log('ajax: ' + json);
+        if (!json) {
+            console.log('No data received - terminating live-data');
+            return;
+        }
+        json = json.replace(/(.*[\]}])\#.*/, '$1');    // remove trailing #comment
+        try {
+            data = JSON.parse( json );
+        } catch (e) {
+            console.log('Error condition detected - terminating live-data');
+            console.log(json);
+            return false;
+        }
+
+        if (typeof data.lastUpdated !== 'undefined') {
+            this.lastUpdated = data.lastUpdated;
+        }
+        if (typeof data.result === 'undefined') {
+            console.log('_live_data_service.php reported an error');
+            console.log(json);
+            return;
+        }
+
+        // regular response:
+        if (typeof data.data === 'undefined') {
+            if (this.debugOutput) {
+                console.log(timeStamp() + ': No new data');
+            }
+
+        } else {
+            var goOn = true;
+            if (typeof liveDataCallback === 'function') {
+                goOn = liveDataCallback( data.data );
+            }
+            if (goOn) {
+                this.updateDOM(data);
+            }
+            if (typeof liveDataPostUpdateCallback === 'function') {
+                liveDataPostUpdateCallback( data.data );
+            }
+        }
+        $('.live-data-update-time').text( timeStamp() );
+        this.updateLiveData();
+    }, // handleAjaxResponse
+
+
+
+
+    updateLiveData: function() {
+        var rootObj = this;
+        var url = appRoot + "_lizzy/extensions/livedata/backend/_live_data_service.php";
+        if (this.paramName) {
+            var paramValue = $( this.paramSource ).text();
+            url = appendToUrl(url, 'dynDataSel=' + this.paramName + ':' + paramValue);
+            if (paramValue !== prevParamValue) {
+                this.lastUpdated = 0;
+                this.prevParamValue = paramValue;
+            }
+        }
+
+        if (this.ajaxHndl !== null){
+            this.ajaxHndl.abort();
+        }
+        this.ajaxHndl = $.ajax({
+            url: url,
+            type: 'POST',
+            data: { ref: rootObj.refs, last: rootObj.lastUpdated },
+            async: true,
+            cache: false,
+        }).done(function ( json ) {
+            return rootObj.handleAjaxResponse(json);
+        });
+    }, // updateLiveData
+
+
+
+
+    abortAjax: function() {
+        if (this.ajaxHndl !== null){
+            this.ajaxHndl.abort();
+            this.ajaxHndl = null;
+            console.log('live-data: last ajax call aborted');
+        }
+        $.ajax({
+            url: appRoot + "_lizzy/_ajax_server.php?abort=true",
+            type: 'GET',
+            cache: false,
+        }).done(function () {
+            console.log('live-data: server process aborted');
+        });
+    } // abortAjax
+
+}); // Object LiveData
+
+
+
+
+$(window).bind('beforeunload', function() {
+    LiveData.abortAjax();
 });
 
 
-
-function abortAjax()
-{
-    if (ajaxHndl !== null){
-        ajaxHndl.abort();
-        ajaxHndl = null;
-        console.log('live-data: last ajax call aborted');
-    }
-    $.ajax({
-        url: appRoot + "_lizzy/_ajax_server.php?abort=true",
-        type: 'GET',
-        cache: false,
-    }).done(function () {
-        console.log('live-data: server process aborted');
-    });
-}
