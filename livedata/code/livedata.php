c24983ac0fd6e70e1963bcba78b352b4596d8eb5
Dieter Stokar <zurisee@gmail.com>
2020-12-31 16:27:55 +0100

    cleanup

--- livedata/code/livedata.php
+++ livedata/code/livedata.php
@@ -2,14 +2,6 @@
 require_once SYSTEM_PATH.'extensions/livedata/code/live-data.class.php';
 
 $page->addModules('~ext/livedata/js/live_data.js');
-$jq = <<<EOT
-
-if ($('[data-lzy-data-ref]').length) {
-    initLiveData();
-}
-
-EOT;
-$page->addJq($jq);
 
 $macroName = basename(__FILE__, '.php');    // macro name normally the same as the file name
 $this->addMacro($macroName, function () {
@@ -24,9 +16,6 @@ $this->addMacro($macroName, function () {
     $this->getArg($macroName, 'targetSelector', '(optional) Id of DOM element(s). If not specified, id will be derived from elementName.  Use format "A|B|C" to specify a list of ids.', false);
     $this->getArg($macroName, 'pollingTime', '(optional) Polling time, i.e. the time the server waits for new data before giving up and responding with "No new data".', false);
     $this->getArg($macroName, 'output', '[true|false] If false, no visible output is rendered, only the mechanism is set up. So, you need to create the DOM elements corresponding to targetSelectors by other means.', true);
-//    $this->getArg($macroName, 'tag', '', '');
-//    $this->getArg($macroName, 'class', '', '');
-//    $this->getArg($macroName, 'mode', '[manual] Manual mode: invoke live-data fields manually. Means, HTML code is created outside of this macro, in particular in case when elementName is specified as a list.', false);
     $this->getArg($macroName, 'callback', '(optional) If defined, the js function will be called before updating the correspondint target value.', false);
     $this->getArg($macroName, 'postUpdateCallback', '(optional) If defined, the js function will be called after updating the correspondint target value.', false);
     $this->disablePageCaching = $this->getArg($macroName, 'disableCaching', '(false) Enables page caching (which is disabled for this macro by default). Note: only active if system-wide caching is enabled.', true);
